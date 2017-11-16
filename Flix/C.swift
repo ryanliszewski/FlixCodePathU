9405ea1d005737cfe1e83d91d69d5afd0e90761d
Ryanliszewski <ryanliszewski@gmail.com>
2017-11-15 16:58:09 -0800

    Completed launch screen with autolayout. Added app icon. Started API client.

---
+++ Flix/C.swift
@@ -0,0 +1,15 @@
+//
+//  C.swift
+//  Flix
+//
+//  Created by Ryan Liszewski on 11/15/17.
+//  Copyright © 2017 ImThere. All rights reserved.
+//
+
+import Foundation
+
+class C {
+  struct movieDatabaseAPI {
+    let key = "a07e22bc18f5cb106bfe4cc1f83ad8ed"
+  }
+}
---
+++ Flix/MovieDatabaseClient.swift
@@ -0,0 +1,13 @@
+//
+//  MovieDatabaseClient.swift
+//  Flix
+//
+//  Created by Ryan Liszewski on 11/15/17.
+//  Copyright © 2017 ImThere. All rights reserved.
+//
+
+import Foundation
+
+class MoviedatabaseClient: URLSession {
+  
+}
